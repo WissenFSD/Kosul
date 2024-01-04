1126288a5a5c6d6ed5a59c378beff50a3c92c259
BRIGHT <BRIGHT@DESKTOP-ST51CUU>
2024-01-04 21:53:18 +0300

    Add project files.

---
+++ Kosul_İfadeleri/Program.cs
@@ -0,0 +1,38 @@
+﻿//
+// koşul  bazı şartlar olduğunda, belirli kodların çalıştırılmasına denir.
+
+//int sayi = 10;
+//int sayi2 = 11;
+
+//bool isok = sayi < sayi2;
+
+//if (isok)
+//{
+//  Console.WriteLine("sayi 2 sayi 1 den büyük");
+
+//}
+//Console.ReadLine();
+
+
+string kullanıcıAdi = "root";
+string sifre = "1010";
+
+Console.WriteLine("Kullanıcı adini giriniz");
+string username=Console.ReadLine();
+
+Console.WriteLine("Şifrenizi giriniz");
+
+string password = Console.ReadLine();
+
+bool girisOk = username == kullanıcıAdi && sifre == password;
+
+if (girisOk)
+{
+
+    Console.WriteLine("Giriş Başarılı");
+}
+else
+{
+
+    Console.WriteLine("Giriş Başarısız");
+}
